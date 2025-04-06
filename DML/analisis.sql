d983b5d9c799bdf4705a86304433b8ccac23f942
Aleksey <boglaev.ale@gmail.com>
2025-04-06 21:59:27 +0300

    Add some dml scripts

--- DML/analisis.sql
+++ DML/analisis.sql
@@ -2,3 +2,19 @@
 select count(customer_pet_breed)
 from mock_data
 where customer_pet_breed is not null;
+
+
+-- анализируем данные по месяцам, тут все как с id в таблицах d_day, d_month
+with sub as (
+	select sale_date as date
+	from mock_data
+	union
+	select product_release_date as date from mock_data 
+	union select product_expiry_date as date from mock_data
+)
+
+select distinct sub.date,
+	extract(DOW from sub.date) + 1 as day,
+	extract(month from sub.date) as month,
+	extract(year from sub.date) as year
+from sub;
--- DML/dml.sql
+++ DML/dml.sql
@@ -52,3 +52,74 @@ sub3 as (
 INSERT INTO d_city (name)
 select distinct * FROM sub3 
 WHERE city is not null; 
+
+
+-- pet type extract
+insert into d_type (type_name)
+select distinct customer_pet_type
+from mock_data
+where customer_pet_type is not null;
+
+
+-- get pet info
+insert into d_pet (name, type_id, breed_id)
+select 
+	data.customer_pet_name,
+	(select id from d_type where type_name = data.customer_pet_type),
+	(select id from d_breed where breed_name = data.customer_pet_breed)
+from mock_data as data
+where data.customer_pet_name is not null;
+
+
+-- get time info
+with sub as (
+	select sale_date as date
+	from mock_data
+	union
+	select product_release_date as date from mock_data 
+	union 
+    select product_expiry_date as date from mock_data
+)
+
+insert into d_time (date, day_id, month_id, year)
+select distinct date,
+	extract (DOW from date) + 1,
+	extract (month from date),
+	extract (year from date)
+from sub;	
+
+
+-- pet category
+insert into d_pet_category (pet_category)
+select distinct pet_category
+from mock_data
+where pet_category is not null;
+
+
+-- get country
+with cntr as (
+	select customer_country as country
+	from mock_data
+	union
+	select seller_country as country
+	from mock_data
+	union
+	select store_country as country
+	from mock_data
+	union
+	select supplier_country as country
+	from mock_data
+)
+
+insert into d_country (name)
+select distinct country
+from cntr
+where country is not null;
+
+
+-- get product color
+insert into d_product_color (color)
+select distinct product_color
+from mock_data
+where product_color is not null;
+
